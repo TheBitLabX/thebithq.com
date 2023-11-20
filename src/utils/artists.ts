9a0b7774b20d35d9a76be1f99591f39929b5dee4
Isaiah Awotide <38901009+McZenith@users.noreply.github.com>
2023-11-20 17:00:46 +0000

    works

--- src/utils/artists.ts
+++ src/utils/artists.ts
@@ -1,26 +1,24 @@
-"use client"
 import { Spotifly } from "spotifly";
 import { artistsSpotifyUrls } from "@/data";
-import { cookies } from "next/headers";
+import React, { useState, useEffect } from 'react';
+var SpotifyWebApi = require('spotify-web-api-node');
 
-
-export const fetchArtistsData = async () => {
-  let dataList = [];
+const useFetchArtistsData = async () => {
   const sp = new Spotifly();
-    for(let artist of artistsSpotifyUrls){
-      const artistData = await sp.getArtist(artist);
-      console.log(artistData.data.artistUnion.profile.name)
-      const data = {
-        id: artist,
-        name: artistData.data.artistUnion.profile.name,
-        cover_img: artistData.data.artistUnion?.visuals.avatarImage.sources[0]?.url,
-        song: artistData.data.artistUnion?.discography.topTracks.items[0]?.track.name,
-        url: artistData.data.artistUnion.sharingInfo.shareUrl,
-      };
-      dataList.push(data);
-      console.log(data)
-    }
-    console.log(dataList);
-    cookies().set("artistsData", JSON.stringify(dataList));
-    console.log(dataList)
-  };
+  let dataList: { id: string; name: string; cover_img: string; song: string; url: string; }[] = [];
+  for (const artist of artistsSpotifyUrls) {
+    const tempData = await sp.getArtist(artist);
+    const newData = {
+      id: artist,
+      name: tempData.data.artistUnion.profile.name,
+      cover_img: tempData.data.artistUnion?.visuals.avatarImage.sources[0]?.url,
+      song: tempData.data.artistUnion?.discography.topTracks.items[0]?.track.name,
+      url: tempData.data.artistUnion.sharingInfo.shareUrl,
+    };
+    dataList.push(newData);
+  }
+  console.log(dataList)
+  return dataList;
+};
+
+export default useFetchArtistsData;
