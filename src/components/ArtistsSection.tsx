a729c1953e7c90a73630a66e6e4d34d7716f4bfa
Isaiah Awotide <38901009+McZenith@users.noreply.github.com>
2024-01-08 00:45:26 +0000

    fixes snap of scrolltab

--- src/components/ArtistsSection.tsx
+++ src/components/ArtistsSection.tsx
@@ -1,7 +1,9 @@
 import { ArtistDetails } from "@/interfaces";
 import ArtistCard from "./ArtistCard";
 async function getData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/sportify`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URL ?? process.env.VERCEL_URL}/api/sportify`
+  );
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
